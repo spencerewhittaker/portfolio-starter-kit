a5d6992abdb3fe5f5e237c72ed9c73cb0814e346
spencerewhittaker <64170534+spencerewhittaker@users.noreply.github.com>
2025-04-09 11:56:12 -0700

    Update page.tsx
    Update blog page title and metadata

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,14 @@
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: 'Spencer Tries Things Out',
+  description: 'Giving it a whirl.',
 }
 
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Spencer's Blog</h1>
       <BlogPosts />
     </section>
   )
