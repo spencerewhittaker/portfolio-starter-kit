3ac313f22fef44dbc459d0b64ee2efd6515d3dcc
spencerewhittaker <64170534+spencerewhittaker@users.noreply.github.com>
2025-04-09 12:50:46 -0700

    Update page.tsx
    Slight text change

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,10 @@ export default function Page() {
         Spencer's Practice Run
       </h1>
       <p className="mb-4">
-        {`As a 6'4 professional athlete I've rarely had need to understand how technology works. 
+        {`As a 6'4 professional athlete I've rarely had the need to understand how technology works. 
+       
         But now it's finally time to see how the other half lives. 
+       
         I'm in the arena. I'm trying things.`}
       </p>
       <div className="my-8">
