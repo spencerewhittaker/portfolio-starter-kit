cc41b2a581d1390f04149efc33fed802e4f2137b
spencerewhittaker <64170534+spencerewhittaker@users.noreply.github.com>
2025-04-09 12:19:37 -0700

    Update page.tsx
    Update home page header and description text

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,12 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Spencer's Practice Run
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`As a 6'4 professional athlete I've rarely had need to understand how technology works. 
+        But now it's finally time to see how the other half lives. 
+        I'm in the arena. I'm trying things.`}
       </p>
       <div className="my-8">
         <BlogPosts />
